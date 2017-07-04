b9cf61196314ac23fd6bd7dec2b0df11ec7107d9
yicheng <963364424@qq.com>
2017-07-05 00:09:57 +0800

    短信提醒1.0

---
+++ app/src/main/java/com/example/fiver/smart_butler/Service/SmsService.java
@@ -0,0 +1,12 @@
+package com.example.fiver.smart_butler.Service;
+/*
+ *  项目名:  Smart_Butler
+ *  包名:  com.example.fiver.smart_butler.Service
+ *  文件名:  SmsService
+ *  创建者:  YYC
+ *  创建时间:  17/7/4 上午11:27
+ *  描述:  TODO
+ */
+
+public class SmsService {
+}
